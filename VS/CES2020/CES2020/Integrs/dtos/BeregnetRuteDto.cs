8803a47d88e42bbd8dc1ba9e3341b60e8d7bb964
Christoffer Nduru <chnd@netcompany.com>
2020-08-27 14:46:22 +0200

    Integrations folder
    Signed-off-by: Christoffer Nduru <chnd@netcompany.com>

---
+++ VS/CES2020/CES2020/Integrs/dtos/BeregnetRuteDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CES2020.Models;
+
+namespace CES2020.Integrs.dto
+{
+    public class BeregnetRuteDto
+    {
+        public float SamletPris { get; set; }
+        public int SamletTid { get; set; }
+        public int Andel { get; set; }
+        public Forsendelse Forsendelse { get; set; }
+    }
+}
