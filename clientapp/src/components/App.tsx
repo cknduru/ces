d8cf61a06e588daed7cdb78ab05f2bae95b44220
Nedim Zahirovic <neza@netcompany.com>
2020-08-27 14:21:41 +0200

    Replace razor page

--- clientapp/src/components/App.tsx
+++ clientapp/src/components/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import Header from './Header';
 
 const App: React.FC = () => {
     return (
-        <div>
-            Telstar Rute!!!
-        </div>
+        <Row gutter={[16, 16]}>
+            <Col span={12}>
+                <Row>
+                    <Header />
+                </Row>
+            </Col>
+        </Row >
     )
 }
 
---
+++ clientapp/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Button } from "antd";
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <>
+            <Button>
+                Rute beregner
+            </Button>
+            <Button>
+                Administration
+            </Button>
+            <Button>
+                Pris administration
+            </Button>
+        </>
+    )
+}
+
+export default Header
