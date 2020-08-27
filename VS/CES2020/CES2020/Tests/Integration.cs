ce636867913cba52e59848773dade3f4d1198edb
Christoffer Nduru <chnd@netcompany.com>
2020-08-28 01:28:04 +0200

    Package configs for test
    Signed-off-by: Christoffer Nduru <chnd@netcompany.com>

---
+++ VS/CES2020/CES2020/Tests/Integration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CES2020.Integrs;
+using NUnit.Framework;
+
+namespace CES2020.Tests
+{        
+    public class Integration
+    {
+        [Test]
+        public void ExternalIntegrationsEAShouldDeliverAnyData()
+        {
+            ConnectionsIntegration ci = new ConnectionsIntegration();
+
+            var ea = ci.GetEastIndiaTradingRoutes();
+
+            Assert.IsNotEmpty(ea);
+        }
+
+        [Test]
+        public void ExternalIntegrationsOAShouldDeliverAnyData()
+        {
+            ConnectionsIntegration ci = new ConnectionsIntegration();
+
+            var oi = ci.GetOceanicRoutes();
+
+            Assert.IsNotEmpty(oi);
+        }
+    }
+}
