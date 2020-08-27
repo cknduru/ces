943d5306fef62bffd90f385151f85dffcb5067d9
Moltved <kam@netcompany.com>
2020-08-27 10:35:04 +0200

    Update Forbindelse.cs

--- VS/CES2020/CES2020/Models/Forbindelse.cs
+++ VS/CES2020/CES2020/Models/Forbindelse.cs
@@ -7,7 +7,17 @@ namespace CES2020.Models
 {
     public class Forbindelse
     {
+        private int id { get; set;}
+        private  float pris { get; set; }
+        private int tid { get; set; }
+        private By fra { get; set;}
+        private By til { get; set; }
+
+        public enum _forbindelsestype { Telstar, Oceanic, East_India}
+
+        public _forbindelsestype forbindelsestype { get;}
 
 
-    }
+       
+}
 }
