227bad725ece648ee928db49d33ad4e946d0e121
Nedim Zahirovic <neza@netcompany.com>
2020-08-27 15:34:07 +0200

    insert react app

---
+++ VS/CES2020/CES2020/telstar-rute.jsx
@@ -0,0 +1,17 @@
+﻿'use strict';
+import App from '../clientapp/src/components/App'
+
+const e = React.createElement;
+
+class TelstarRute extends React.Component
+{
+    render()
+    {
+        return e(
+            <App />
+        );
+    }
+}
+
+const domContainer = document.querySelector('#telstar-rute');
+ReactDOM.render(e(TelstarRute), domContainer);
