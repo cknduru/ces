a7f04e4ecf23db1389f0144ba4657fc1a9dc0c91
Moltved <kam@netcompany.com>
2020-08-27 10:22:03 +0200

    classes
    By og forbindelse

---
+++ VS/CES2020/CES2020/Models/By.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CES2020.Models
+{
+    public class By
+    {
+        private int Id { get; set; }
+        private string name { get; set; }
+        
+
+    }
+}
