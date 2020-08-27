68a0dd2aeeef9c02f3d84d32c0b39ac3c95b5076
Daniel Cohen Larsen <dcl@netcompany.com>
2020-08-27 13:33:30 +0200

    Repositories added

---
+++ VS/CES2020/CES2020/Repositories/ByRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CES2020.Repositories
+{
+    public class ByRepository
+    {
+      
+    }
+}
