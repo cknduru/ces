6b0d1ef45352fcbeb83fac34b6d3235246b49d16
William Lund Sommer <wls@netcompany.com>
2020-08-28 00:23:37 +0200

    Update PrisadministrationService.cs

--- VS/CES2020/CES2020/Services/PrisadministrationService.cs
+++ VS/CES2020/CES2020/Services/PrisadministrationService.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using CES2020.Repositories;
 
 namespace CES2020.Services
 {
     public class PrisadministrationService
     {
+        private readonly KonfigurationRepository konfigurationRepository;
+
+        public PrisadministrationService()
+        {
+            this.konfigurationRepository = new KonfigurationRepository();
+        }
+
+        public void SetTelstarSegmentPris(float pris)
+        {
+            konfigurationRepository.SetTelstarSegmentPris((double) pris);
+        }
     }
 }
