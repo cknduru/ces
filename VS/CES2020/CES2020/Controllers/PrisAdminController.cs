ad4b366826ed5e5cbf9356e282847c1ccdd69761
Moltved <kam@netcompany.com>
2020-08-28 02:16:38 +0200

    lol

---
+++ VS/CES2020/CES2020/Controllers/PrisAdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CES2020.Integrs;
+using CES2020.Integrs.dto;
+using CES2020.Models;
+using CES2020.Repositories;
+
+namespace CES2020.Controllers
+{
+    public class PrisAdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult PrisAdmin()
+        {
+            var konfiguration = new KonfigurationRepository().Get();
+            return View(konfiguration);
+        }
+
+    }
+}
